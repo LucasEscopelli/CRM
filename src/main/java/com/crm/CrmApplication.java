54843e3c2da9dd6e3aa534eabe7f2ec582afa01a
guilherme.kirsch <guilherme.kirsch@animati.com.br>
2023-07-10 11:01:06 -0300

    remove BD

--- src/main/java/com/crm/CrmApplication.java
+++ src/main/java/com/crm/CrmApplication.java
@@ -2,9 +2,7 @@ package com.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan("com.crm.cliente.*")
 @SpringBootApplication
 public class CrmApplication {
 
